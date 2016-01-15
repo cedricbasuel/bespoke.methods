6b830c61909c3aef23c234d511761052de5b0783
cedricbasuel <cedric.basuel@gmail.com>
2016-01-15 11:53:30 +0800

    Update articleMatching.R

--- articleMatching.R
+++ articleMatching.R
@@ -32,6 +32,28 @@ names(article_scores) = c("08 jan Article","Number of matches","Words matched","
 articleString = list()
 article_annotations = list()
 article_docs = list()
+
+word_an = Maxent_Word_Token_Annotator()
+sent_an = Maxent_Sent_Token_Annotator()
+money_an = Maxent_Entity_Annotator(kind = "money")
+date_an = Maxent_Entity_Annotator(kind = "date")
+location_an = Maxent_Entity_Annotator(kind = "location")
+#misc_an = Maxent_Entity_Annotator(kind = "misc")
+org_an = Maxent_Entity_Annotator(kind = "org")
+pipeline = list(sent_an,word_an,money_an,date_an,location_an,org_an)
+
+entities = function(doc, kind) {
+  s <- doc$content
+  a <- annotations(doc)[[1]]
+  if(hasArg(kind)) {
+    k <- sapply(a$features, `[[`, "kind")
+    s[a[k == kind]]
+  } else {
+    s[a[a$type == "entity"]]
+  }
+}
+
+
 for(j in 1:length(article_list)){
   scoreKey = c()
   truematch = c()
@@ -53,4 +75,14 @@ for(j in 1:length(article_list)){
   article_scores[j,5] = try(as.String(entities(article_docs[[j]],kind = "location")))
   article_scores[j,6] = try(as.String(entities(article_docs[[j]],kind = "organization")))
 }
+txt_files_index = substr(txt_files,1,2)
+txt_files_index = gsub(" ","",txt_files_index)
+txt_files_index = as.numeric(txt_files_index)
+newstitles = read.csv("newslinkstoscrape_Jan13.csv")
+article_scores$"Headline" = NA
+for(i in 1:nrow(article_scores)){
+  row = txt_files_index[i]
+  article_scores$Headline[i] = as.String(newstitles[row,2])
+}
+
 write.csv(article_scores,"article_scores.csv")
